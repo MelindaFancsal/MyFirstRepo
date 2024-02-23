7fe3dab666a360db989274c6e1072baaa98b0194
Melinda Fancsal <melinda.fancsal@gmail.com>
2024-02-23 19:28:05 +0200

    adding some new feature text

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world!");
 
         System.out.println("Adding some new code");
+
+        System.out.println("Adding some new code");
     }
 }
